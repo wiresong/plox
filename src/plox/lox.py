fe2f4c3ca19fcd36581372ce737ebdde1be0d5e6
wiresong <wiresong@wiresong.me>
2024-07-11 16:35:57 +0530

    Slight fixes to error handling

--- src/plox/lox.py
+++ src/plox/lox.py
@@ -2,48 +2,49 @@ import sys
 
 from .interpreter import Interpreter
 from .parser import Parser
-from .printer import Printer
 from .resolver import Resolver
 from .tokenizer import Tokenizer
 
 
 class Lox:
     def __init__(self):
-        self.hadError = False
-        self.hadRuntimeError = False
+        self.had_error = False
+        self.had_runtime_error = False
         self.interpreter = Interpreter(self)
 
     def run_file(self, file):
         with open(file, encoding="utf-8") as f:
             self.run(f.read())
+        if self.had_error:
+            sys.exit(65)
 
     def run_prompt(self):
         while True:
-            self.hadError = False
-            self.hadRuntimeError = False
             print(">")
             s = input()
             self.run(s)
+            self.had_error = False
+            self.had_runtime_error = False
+
 
     def run(self, s):
-        # Todo reuse tokenizer
         tokens = Tokenizer(self, s).tokenize()
         parser = Parser(self, tokens)
         statements = parser.parse()
-        if self.hadError:
+        if self.had_error:
             return
-        if self.hadRuntimeError:
+        if self.had_runtime_error:
             sys.exit(70)
         resolver = Resolver(self, self.interpreter)
         resolver.resolve_statements(statements)
-        if self.hadError:
+        if self.had_error:
             return
         self.interpreter.interpret(statements)
 
     def error(self, line, s):
-        self.hadError = True
+        self.had_error = True
         print(f"Error at line {line}: {s}")
 
     def runtime_error(self, error):
-        self.hadRuntimeError = True
-        print(f"Error with token {error.token}: {error}")
+        self.had_runtime_error = True
+        print(f"Runtime error at line {error.token.line}: {error}")
